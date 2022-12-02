c7b550b0a6fce8d9a0945a9f04bc5c43cd05b10b
Svetlana Govyazina <govyazina@gmail.com>
2022-12-02 19:38:51 +0200

    added function for task 3

--- assets/task3.js
+++ assets/task3.js
@@ -1,7 +1,17 @@
-
-
 function formatDate(date) {
-
+    let diffDate = new Date() - date;
+    if(diffDate < 1000){
+        return "прямо сейчас"
+    }
+    else if (diffDate < 1000*60){
+        return diffDate/1000 + ' сек. назад';
+    }
+    else if (diffDate < 1000*60*60){
+        return diffDate/1000/60 + ' мин. назад';
+    }
+    else {
+        return date.getDate() + '.' + (date.getMonth() + 1) + '.' + (date.getFullYear() % 100) + ' ' + date.getHours() + ':' + date.getMinutes();
+    }
 }
 
 module.exports = formatDate;
--- assets/task3.test.js
+++ assets/task3.test.js
@@ -1,3 +1,7 @@
 const date = require('./task3');
 
+
 test('test date', () => {expect(date(new Date(new Date - 1))).toBe('прямо сейчас')});
+test('test date2', () => {expect(date(new Date(new Date - 30 * 1000))).toBe('30 сек. назад')});
+test('test date3', () => {expect(date(new Date(new Date - 5 * 60 * 1000))).toBe('5 мин. назад')});
+test('test date4', () => {expect(date(new Date(new Date - 86400* 4 * 1000))).toMatch(/^\d{1,2}\.\d{1,2}\.\d{1,2} \d{1,2}:\d{1,2}$/)})
