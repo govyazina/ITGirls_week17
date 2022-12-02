70fa6d6565c9230c095bda1cd6c625ef63d890ac
Svetlana Govyazina <govyazina@gmail.com>
2022-12-02 16:55:52 +0200

    added function for show message and some styles changed

---
+++ assets/task2.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('#comment');
+const wrapper = document.querySelector('.wrap_messages');
+
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        sendMessage();
+    }
+})
+
+function sendMessage() {
+    const message = input.value;
+    const finalMessage = checkSpam(message);
+    showMessage(finalMessage);
+    input.value = '';
+}
+
+function checkSpam(str) {
+    return str;
+}
+
+function showMessage(str) {
+    wrapper.insertAdjacentHTML("beforeend", `<div class="message">${str}</div>`);
+}
